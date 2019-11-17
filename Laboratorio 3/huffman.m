52b9da78ca7d4154a495922527dcf848c56217f2
jdrodriguezrui <jdrodriguezrui@unal.edu.co>
2019-11-16 22:56:15 -0500

    Cambiecito xd

--- Laboratorio 3/huffman.m
+++ Laboratorio 3/huffman.m
@@ -1,10 +1,11 @@
 function binaryMatrix = huffman(vector)
-  #M = [0.3 0.25 0.2 0.12 0.08 0.05] prueba
+  #M = [0.3 0.25 0.2 0.12 0.08 0.05] #prueba
   len = length(vector);
   vector = sort(vector);
   for i = 1:len
     value.p = vector(i);
     value.b = auxHuffman(vector,i);
     binaryMatrix(end+1) = value;
+    value
   endfor
   
